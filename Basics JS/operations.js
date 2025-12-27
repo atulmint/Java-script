20cef462ccb816b360a6593092b87ccafe206c61
atulmint <133959520+atulmint@users.noreply.github.com>
2025-12-27 13:27:51 +0000

    Next topics

---
+++ Basics JS/operations.js
@@ -0,0 +1,17 @@
+//Prefix and Postfix
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+let Z = 3;
+const A = ++Z;
+// Z is 4; A is 4
+
+let Q2 = 3n;
+const S2 = ++Q2;
+// Q2 is 4n; S2 is 4n
